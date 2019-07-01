d12af8e20dfe7be5b05aff6dcf3eee5e5a18d279
gz-liufu <lucien_20@163.com>
2019-07-01 21:39:08 +0800

    master

---
+++ message-thrift-python-service/mesage/message_serice.py
@@ -0,0 +1,40 @@
+from mesage.api import MessageService
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift.server import TServer
+
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+sender='imoocd@163.com'
+authCode='a111111'
+
+class MessageServiceHandler:
+    def sendMobileMessage(self, mobile, message):
+        print "sendMovileMessage，mobile:"+mobile+",message:"+message
+        return True
+
+    def sendEmailMessae(self, email, message):
+        print "sendEmailMessage，email:"+email+",message:"+message
+        messageObj=MIMEText(message,'plain','utf-8')
+        messageObj['From']=sender
+        messageObj['To'] = email
+        messageObj['Subject'] = Header('慕课网邮件' , 'utf-8')
+        try:
+            smtpObj = smtplib.SMTP('smpt.163.com')
+            smtpObj.login(sender, authCode)
+            smtpObj.sendmail(sender,[email],)
+        return True
+if __name__ == '__main__':
+    handler=MessageServiceHandler()
+    processor=MessageService.Processor(handler)
+    transport=TSocket.TServerSocket("localhost" , "9090")
+    tfactory=TTransport.TFramedTransportFactory()
+    pfactory=TBinaryProtocol.TBinaryProtocolFactory()
+
+    server=TServer.TSimpleServer(processor,transport,tfactory,pfactory)
+    print "python thrift server start"
+    server.serve()
+    print "python thrift server end"
